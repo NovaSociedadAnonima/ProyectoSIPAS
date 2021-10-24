d7c35bafa869ab15dea59b9ecb8a60bac80e3e03
SantiagoSoto5555 <92376120+SantiagoSoto5555@users.noreply.github.com>
2021-10-24 15:18:59 -0300

    Delete Lista.java

--- src/proyectofinal2/Lista.java
+++
@@ -1,8 +0,0 @@
-
-package proyectofinal2;
-import java.util.ArrayList;
-
-public class Lista {
-     private ArrayList<Alumno> Alumnos= new ArrayList<>();
-    //Curso PerteneceCurso= new Curso();
-}
