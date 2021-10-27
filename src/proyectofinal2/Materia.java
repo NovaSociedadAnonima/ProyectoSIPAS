6ce262e9120d086293422daa3d3620996cf357d3
SantiagoSoto5555 <92376120+SantiagoSoto5555@users.noreply.github.com>
2021-10-27 11:28:47 -0300

    Update Materia.java

--- src/proyectofinal2/Materia.java
+++ src/proyectofinal2/Materia.java
@@ -1,28 +1,35 @@
-package proyectofinal2;
+package ProyectoFinAño;
 
 import java.util.ArrayList;
 
 public class Materia {
 
     
+   
+
     private int IdCurso;
     private String Nombre;
     private Profesor ProfesorE;
 
-    private ArrayList<Integer> NoPresentes = new ArrayList();
+   private ArrayList<int[]> NoPresentes=new ArrayList<int[]>();
+      private ArrayList<int[]> LlegoTarde=new ArrayList<int[]>();
 
     public Materia(String Nombre, Profesor ProfesorE, int IdCurso) {
         this.Nombre = Nombre;
         this.ProfesorE = ProfesorE;
         this.IdCurso = IdCurso;
     }
-public int getIdCurso() {
+
+ 
+
+    public int getIdCurso() {
         return IdCurso;
     }
 
     public void setIdCurso(int IdCurso) {
         this.IdCurso = IdCurso;
     }
+
     public String getNombre() {
         return Nombre;
     }
@@ -38,13 +45,23 @@ public int getIdCurso() {
     public void setProfesorE(Profesor ProfesorE) {
         this.ProfesorE = ProfesorE;
     }
-
-    public ArrayList getNoPresentes() {
+ public ArrayList<int[]> getNoPresentes() {
         return NoPresentes;
     }
 
-    public void setNoPresentes(ArrayList NoPresentes) {
+  
+    public void setNoPresentes(ArrayList<int[]> NoPresentes) {
         this.NoPresentes = NoPresentes;
     }
+
+   
+    public ArrayList<int[]> getLlegoTarde() {
+        return LlegoTarde;
+    }
+
+    public void setLlegoTarde(ArrayList<int[]> LlegoTarde) {
+        this.LlegoTarde = LlegoTarde;
+    }
+
 
 }
